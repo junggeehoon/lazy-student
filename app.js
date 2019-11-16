9a05349310e0104469f06fc6bf5824f90d0c6ef0
junggeehoon <junggeehoon@gmail.com>
2019-11-15 19:14:55 -0800

    final

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const process = require('./process');
 const express = require('express');
 const parser = require('body-parser');
 const app = express();
@@ -10,11 +11,22 @@ app.get('/', (req, res) => {
 })
 
 app.post('/grade', (req, res) => {
-  res.send(req.body);
-  console.log(req.body)
+  const data = {};
+  const target = req.body["targetScore"];
+  delete req.body.targetScore;
+  for (let i = 0; i < req.body["category"].length; i++) {
+    data[req.body["category"][i]] = [];
+    data[req.body["category"][i]].push(Number(req.body["currentPts"][i]));
+    data[req.body["category"][i]].push(Number(req.body["totalPts"][i]));
+    data[req.body["category"][i]].push(Number(req.body["weight"][i]));
+    data[req.body["category"][i]].push(Number(req.body["effort"][i]));
+    data[req.body["category"][i]].push(Number(target));
+  }
+  console.log(process(data));
+  res.send(process(data));
 })
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
--- process.js
+++ process.js
@@ -1,32 +1,25 @@
-const data = {
-  "homework": [540, 560, 60, 80],
-  "lecture": [85, 100, 35, 10],
-  "lab": [89, 100, 5, 0]
-}
+module.exports = data => {
+  let totalGrade = 0;
+  let totalEffort = 0;
 
-const targetGrade = 95;
+  for (const key in data) {
+    const currentPoint = data[key][0];
+    const availablePoint = data[key][1];
+    const weight = data[key][2];
+    const effort = data[key][3];
+    const grade = (currentPoint / availablePoint) * weight;
+    totalGrade += grade;
+    totalEffort += effort;
+  }
 
-let totalGrade = 0;
-let totalEffort = 0;
-
-for (const key in data) {
-  const currentPoint = data[key][0];
-  const availablePoint = data[key][1];
-  const weight = data[key][2];
-  const effort = data[key][3];
-  const grade = (currentPoint / availablePoint) * weight;
-  totalGrade += grade;
-  totalEffort += effort;
-}
-
-const calculateGrade = theData => {
   let result = {};
-  for (const key in theData) {
+  for (const key in data) {
     result[key] = [];
-    const currentPoint = theData[key][0]; // 540
-    const availablePoint = theData[key][1]; // 560
-    const weight = theData[key][2]; // 60
-    const effort = theData[key][3]; // 80
+    const currentPoint = data[key][0]; // 540
+    const availablePoint = data[key][1]; // 560
+    const weight = data[key][2]; // 60
+    const effort = data[key][3]; // 80
+    const targetGrade = data[key][4];
 
     const effortGrade = (effort / totalEffort) * (targetGrade - totalGrade); //  80 / 90   * 95 - 92.057
     const grade = (currentPoint / availablePoint) * weight ; // 96.4 * 0.6
@@ -37,6 +30,3 @@ const calculateGrade = theData => {
   }
   return result;
 }
-
-const output = calculateGrade(data);
-console.log(output);
